5d221d617e3c501239d29b939fc5dc11625d4cfd
Christopher Lawrence <chrislawrence05@gmail.com>
2024-10-13 08:10:24 -0400

    add circleshare

--- main.py
+++ main.py
@@ -2,15 +2,34 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, 'containers'):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen: pygame.surface.Surface):
+        pass
+
+    def update(self, dt: int):
+        pass
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +37,8 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
+        # limit the fps to 60
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
